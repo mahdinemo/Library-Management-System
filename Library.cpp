1253a5034e10785147eef258a86b81fc52b2185c
mahdinemo <m.mahdi.m8279@gmail.com>
2024-05-02 02:16:39 +0430

    Adding the cpp file and also creating the main menu of the program

---
+++ Library.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+		
+	int Menu_Choice;
+	cout<<"\n\t\t\t\t***************** Library Management System *****************"<<endl;
+	cout<<"\n\t\t\t\t\t\t\t1 : Add book";
+	cout<<"\n\t\t\t\t\t\t\t2 : Search";
+	cout<<"\n\t\t\t\t\t\t\t3 : Edit";
+	cout<<"\n\t\t\t\t\t\t\t4 : List of whole books";
+	cout<<"\n\t\t\t\t\t\t\t5 : Counter for book categories";
+	cout<<"\n\t\t\t\t\t\t\t6 : Exit"<<endl;
+	cout<<"\n\t\t\t\t\t\t\tEnter Your Choice : ";
+	cin >> Menu_Choice;
+
+
+
+
+
+
+cin.ignore();
+cin.get();
+return 0;
+}
