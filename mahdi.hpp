ae7f34265cf7882954f02bfa30fb5920baf66c48
mahdinemo <m.mahdi.m8279@gmail.com>
2024-05-04 11:08:00 +0430

    There was a problem on this code.if user enters 11 the program would consider it as 1 because I used char not string . So I changed char into string to prevent this kind of troubles.

--- mahdi.hpp
+++ mahdi.hpp
@@ -8,7 +8,7 @@ using namespace std;
 
 //first of all I need two functions for my error handlings. one for digits and the other one is for chars. 
 bool valid_input = false;
-char Menu_Choice;
+string Menu_Choice;
 int choice;
 
 int digit_error(int begin , int end) //they show us the starting and finishing points of the interval 
@@ -17,11 +17,11 @@ int digit_error(int begin , int end) //they show us the starting and finishing p
 	{
 		cout<<"\n\t\t\t\t\t\t\tEnter Your Choice : ";
 		cin>>Menu_Choice;
-		if (!isdigit(Menu_Choice)) {
+		if (!isdigit(Menu_Choice[0])) {
             cout << "\n\t\t\t\t\t\t\tError: Invalid input. Please Enter a Valid Digit From "<<begin<<" 'till "<<end<< endl;
             continue;
         }
-        choice = Menu_Choice - '0'; // converting the character into an integer.
+        choice = stoi(Menu_Choice); // converting the character into an integer.
         
         if(choice < 1 || choice > 6)
         {
