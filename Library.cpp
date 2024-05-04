609af12ddf9d1e00e581fff19e6232b086004867
mahdinemo <m.mahdi.m8279@gmail.com>
2024-05-04 11:05:39 +0430

    writing the switch case for the Main Menu and also starting riting the basic of the library class and it's functions

--- Library.cpp
+++ Library.cpp
@@ -2,6 +2,29 @@
 #include "mahdi.hpp"
 
 using namespace std;
+
+class library
+{
+	public:
+		string book_name;
+		string writer_name;
+		string title;
+		int cost;
+		long int id;
+		
+		//===========================
+		
+		void add_book();
+		void search_by_id();
+		void search_by_name();
+		void search_by_title();
+		void edit_by_id();
+		void list();
+		void count_books();
+}
+
+
+
 int main()
 {
 		
@@ -15,7 +38,53 @@ int main()
 	cout<<"\n\t\t\t\t\t\t\t6 : Exit"<<endl;
 	
 	//I want to hasndle the digits error in just one line of code .
+	
 	digit_error(1 , 6);
+	Menu_Choice = digit_error(1 , 6);
+	
+	switch(Menu_Choice)
+	{
+		case 1:
+			{
+				system("CLS");
+				cout<<"\n\t\t\t\t\t\t\tAdd Book";
+				break;
+			}
+			
+		case 2:
+			{
+				system("CLS");
+				cout<<"\n\t\t\t\t\t\t\tSearch";
+				break;
+			}
+			
+		case 3:
+			{
+				system("CLS");
+				cout<<"\n\t\t\t\t\t\t\tEDIT";
+				break;
+			}
+			
+		case 4:
+			{
+				system("CLS");
+				cout<<"\n\t\t\t\t\t\t\tList Of Whole Books";
+				break;
+			}
+		
+		case 5:
+			{
+				system("CLS");
+				cout<<"\n\t\t\t\t\t\t\tCounter For Books";
+				break;
+			}
+			
+		case 6:
+			{
+				exit(0);
+			}
+			
+	}
 
 
 
